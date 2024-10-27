433e776a0a2673e98e46ca03124e8dbc054fe0e1
Martinpasaribu <83689864+Martinpasaribu@users.noreply.github.com>
2024-10-28 03:30:58 +0700

    update token

--- Server/controllers/RefreshToken.js
+++ Server/controllers/RefreshToken.js
@@ -9,16 +9,16 @@ dotenv.config();
 export  const refreshToken = async (req, res) => {
     try {
         const refreshToken = req.cookies.refreshToken;
-        if(!refreshToken) return res.sendStatus(401);
+        if(!refreshToken) return res.status(401).json({message: " Session at cookies empty "});
         const user = await Users.findAll( {
             where : {
                 refresh_token: refreshToken
             }
         });
-        if(!user[0]) return res.sendStatus(403);
+        if(!user[0]) return res.status(401).json({message: "User noAuth, Login again "});
         jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => 
         {
-            if(err) return res.sendStatus(403);
+            if(err) return res.status(401).json({message: "Token No Veryfy, Login again "});
             const userId = user[0].id;
             const name = user[0].name;
             const email = user[0].email;
