7ab770a42177f2c95bf111977a84a464e3ede2c5
Martinpasaribu <83689864+Martinpasaribu@users.noreply.github.com>
2024-10-28 04:20:51 +0700

    update token5

--- Server/controllers/Auth.js
+++ Server/controllers/Auth.js
@@ -54,7 +54,6 @@ export const Login = async (req, res) => {
 
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
-            secure: auto,
             maxAge: 24 * 60 * 60 * 1000, 
         });
 
--- Server/controllers/RefreshToken.js
+++ Server/controllers/RefreshToken.js
@@ -9,16 +9,16 @@ dotenv.config();
 export  const refreshToken = async (req, res) => {
     try {
         const refreshToken = req.cookies.refreshToken;
-        if(!refreshToken) return res.sendStatus(401);
+        if(!refreshToken) return res.status(401).json({ message: 'Session cookies empty' });
         const user = await Users.findAll( {
             where : {
                 refresh_token: refreshToken
             }
         });
-        if(!user[0]) return res.sendStatus(403);
+        if(!user[0]) return res.status(401).json({ message: 'User  empty' });
         jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => 
         {
-            if(err) return res.sendStatus(403);
+            if(err) return res.status(401).json({ message: 'refreshToken not verify' });
             const userId = user[0].id;
             const name = user[0].name;
             const email = user[0].email;
