ac23b7937b568e369346a51be63c826cbb41d0de
Martinpasaribu <83689864+Martinpasaribu@users.noreply.github.com>
2024-10-28 06:09:16 +0700

    update session store

--- Server/index.js
+++ Server/index.js
@@ -45,9 +45,9 @@ testConnection();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST ,
-    user: process.env.DB_USER ,
-    password: process.env.DB_PASS ,
-    database: process.env.DB_NAME ,
+    user: process.env.DB_USERNAME ,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_DATABASE ,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
