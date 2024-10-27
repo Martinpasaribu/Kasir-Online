ec8c73def40f2b2948556bd9f9316aa1bf7780d3
Martinpasaribu <83689864+Martinpasaribu@users.noreply.github.com>
2024-10-28 04:50:05 +0700

    update cookies2

--- Server/controllers/Auth.js
+++ Server/controllers/Auth.js
@@ -52,12 +52,18 @@ export const Login = async (req, res) => {
             },
         });
 
-        res.cookie('refreshToken', refreshToken, {
-            httpOnly: true,
-            secure: true,
-            maxAge: 24 * 60 * 60 * 1000, 
-        });
+        // res.cookie('refreshToken', refreshToken, {
+        //     httpOnly: true,
+        //     secure: true,
+        //     maxAge: 24 * 60 * 60 * 1000, 
+        // });
 
+        res.cookie('refreshToken', refreshToken, { 
+            httpOnly: true, 
+            secure: true, 
+            sameSite: 'None', 
+            maxAge: 24 * 60 * 60 * 1000 
+        });
 
         const decodedRefreshToken = jwtDecode(refreshToken);
         const expiresIn = decodedRefreshToken.exp;
